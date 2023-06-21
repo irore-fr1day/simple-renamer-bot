4f12ff3830a19c707ffbf51b8d4412a69216c342
irore-fr1day <131448923+irore-fr1day@users.noreply.github.com>
2023-06-22 00:56:27 +0500

    Update start_text.py

--- main/start_text.py
+++ main/start_text.py
@@ -12,8 +12,6 @@ async def start_cmd(bot, msg):
         InlineKeyboardButton("🖥️ Получить доступ", url="https://t.me/fr1day_pon")
     ]])
     if msg.from_user.id != ADMIN:
-        continue
-    if msg.from_user.id != ACCESS:
         return await msg.reply_text(text=txt, reply_markup=btn, disable_web_page_preview = True)
     await start(bot, msg, cb=False)
 
