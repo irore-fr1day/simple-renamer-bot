aa0c7534aa692e4732fbe83e6cac672a410d2c5d
irore-fr1day <131448923+irore-fr1day@users.noreply.github.com>
2023-06-22 01:00:04 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,7 @@ from os import environ
 API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-ACCESS = int(environ.get("ACCESS", ""))      
+ADMIN = int(environ.get("ADMIN", ""))      
 CAPTION = environ.get("CAPTION", "")
 
 # for thumbnail ( back end is fr1day brain 😉)
