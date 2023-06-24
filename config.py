5b593509fb0ae8c0c4277c64c80d1c9db148032b
irore-fr1day <131448923+irore-fr1day@users.noreply.github.com>
2023-06-24 15:04:40 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,9 @@ from os import environ
 API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))      
+ADMIN = int(environ.get("ADMIN", ""))        
+ACCESS = int(environ.get("ACCESS", ""))   
 CAPTION = environ.get("CAPTION", "")
 
-# for thumbnail ( back end is fr1day brain 😉)
+
 DOWNLOAD_LOCATION = "./DOWNLOADS"
