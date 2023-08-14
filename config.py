3001dde1603351d2554004f4f2ce1ab324a3f30f
irore-fr1day <131448923+irore-fr1day@users.noreply.github.com>
2023-08-14 13:35:06 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import environ
 API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = 5648834135        
+ADMIN = 904912786        
 ACCESS = 0
 CAPTION = environ.get("CAPTION", "")
 
